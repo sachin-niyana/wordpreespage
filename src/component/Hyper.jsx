cea658e126795f3090dc68a99de954e5f76ca812
sachin-niyana <sachinjakhar100@gmail.com>
2023-09-05 12:52:19 +0530

    change-card

---
+++ src/component/Hyper.jsx
@@ -0,0 +1,29 @@
+import contect from '../assets/png/contect.webp'
+import box from '../assets/png/box.webp'
+import headphone from '../assets/png/headphone.webp'
+
+export const card = [
+    {
+        id: 1,
+        img: contect,
+        heading: "Enterprise WordPress Solutions",
+        peragraph: "Pretium, a et sapien non nunc libero. Id dolor sed quis convallis turpis purus feugiat nibh eu. Auctor cras hac ac, sociis diam montes, turpis commodo elit.",
+        link: "Learn More",
+    },
+    {
+        id: 2,
+        img: box,
+        heading: "Enterprise WordPress Solutions",
+        peragraph: "Pretium, a et sapien non nunc libero. Id dolor sed quis convallis turpis purus feugiat nibh eu. Auctor cras hac ac, sociis diam montes, turpis commodo elit.",
+        link: "Learn More",
+    },
+    {
+        id: 3,
+        img: headphone,
+        heading: "Enterprise WordPress Solutions",
+        peragraph: "Pretium, a et sapien non nunc libero. Id dolor sed quis convallis turpis purus feugiat nibh eu. Auctor cras hac ac, sociis diam montes, turpis commodo elit.",
+        link: "Learn More",
+    },
+]
+
+
--- src/component/Integer.jsx
+++ src/component/Integer.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import contect from '../assets/png/contect.webp'
-import box from '../assets/png/box.webp'
-import headphone from '../assets/png/headphone.webp'
 import { Col, Container, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { card } from './Hyper'
 
 const Integer = () => {
     return (
@@ -11,54 +9,27 @@ const Integer = () => {
             <Container>
                 <h3 className='ff_konexy fs_48 fw_400 fc_black'>Integer et nisl non</h3>
                 <Row className='d-flex justify-content-center'>
-                    <Col lg={4} sm={6} className='d-flex justify-content-center' data-aos="flip-left" data-aos-duration="1500">
-                        <div className='card-1 cursor mt-5'>
-                            <div className='d-flex justify-content-center mt-5'>
-                                <img src={contect} alt="contect" />
-                            </div>
-                            <div className='d-flex justify-content-center text-center'>
-                                <h4 className='ff_inter fw_600 fs_24 fc_black mt-5'>Enterprise WordPress Solutions</h4>
-                            </div>
-                            <div className='d-flex justify-content-center text-center mt-3'>
-                                <p className='ff_gilory fw_400 fs_16 fc_gray max-w-300 ff_gilroy'>Pretium, a et sapien non nunc libero. Id dolor sed quis convallis turpis purus feugiat nibh eu. Auctor cras hac ac, sociis diam montes, turpis commodo elit.</p>
-                            </div>
-                            <div className='d-flex justify-content-center mb-4'>
-                                <Link className='ff_gilory fw_600 fs_16 fc_blue ff_gilroy'>Learn More</Link>
-                            </div>
-                        </div>
-                    </Col>
-                    <Col lg={4} sm={6} className='d-flex justify-content-center' data-aos="flip-left" data-aos-duration="1500">
-                        <div className='card-1 cursor mt-5'>
-                            <div className='d-flex justify-content-center mt-5'>
-                                <img src={box} alt="contect" />
-                            </div>
-                            <div className='d-flex justify-content-center text-center'>
-                                <h4 className='ff_inter fw_600 fs_24 fc_black mt-5'>Small to Mid Size Business Development		</h4>
-                            </div>
-                            <div className='d-flex justify-content-center text-center mt-3'>
-                                <p className='ff_gilory fw_400 fs_16 fc_gray max-w-300 ff_gilroy '>Et nunc fermentum id amet tempus, elementum. Eget ac ultrices tortor, mattis amet blandit aenean amet. Massa diam lacus, aliquam neque, cursus.</p>
-                            </div>
-                            <div className='d-flex justify-content-center mb-4'>
-                                <Link className='ff_gilory fw_600 fs_16 fc_blue ff_gilroy'>Learn More</Link>
-                            </div>
-                        </div>
-                    </Col>
-                    <Col lg={4} sm={6} className='d-flex justify-content-center' data-aos="flip-left" data-aos-duration="1500">
-                        <div className='card-1 cursor mt-5'>
-                            <div className='d-flex justify-content-center mt-5'>
-                                <img src={headphone} alt="contect" />
-                            </div>
-                            <div className='d-flex justify-content-center text-center'>
-                                <h4 className='ff_inter fw_600 fs_24 fc_black mt-5'>Support & Maintenance	</h4>
-                            </div>
-                            <div className='d-flex justify-content-center text-center mt-3'>
-                                <p className='ff_gilory fw_400 fs_16 fc_gray max-w-300 ff_gilroy'>Ac donec ipsum fringilla tortor, consectetur etiam sociis amet quis. Viverra volutpat pretium non in fusce tellus sed urna. Pharetra ullamcorper et donec aliquam.</p>
-                            </div>
-                            <div className='d-flex justify-content-center mb-4'>
-                                <Link className='ff_gilory fw_600 fs_16 fc_blue ff_gilroy'>Learn More</Link>
-                            </div>
-                        </div>
-                    </Col>
+                    {card.map((data) => {
+                        return (
+                            <Col lg={4} sm={6} className='d-flex justify-content-center' data-aos="flip-left" data-aos-duration="1500">
+                                <div className='card-1 cursor mt-5'>
+                                    <div className='d-flex justify-content-center mt-5'>
+                                        <img src={data.img} alt="contect" />
+                                    </div>
+                                    <div className='d-flex justify-content-center text-center'>
+                                        <h4 className='ff_inter fw_600 fs_24 fc_black mt-5'>{data.heading}</h4>
+                                    </div>
+                                    <div className='d-flex justify-content-center text-center mt-3'>
+                                        <p className='ff_gilory fw_400 fs_16 fc_gray max-w-300 ff_gilroy'>{data.peragraph}</p>
+                                    </div>
+                                    <div className='d-flex justify-content-center mb-4'>
+                                        <Link className='ff_gilory fw_600 fs_16 fc_blue ff_gilroy'>{data.link}</Link>
+                                    </div>
+                                </div>
+                            </Col>
+                        )
+                    })}
+
                 </Row>
             </Container>
         </div>
