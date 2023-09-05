8c6cc5e33622beceba0eccdd60e3cb4719b64333
sachin-niyana <sachinjakhar100@gmail.com>
2023-09-05 14:12:50 +0530

    changetext

--- src/component/Hyper.jsx
+++ src/component/Hyper.jsx
@@ -13,15 +13,15 @@ export const card = [
     {
         id: 2,
         img: box,
-        heading: "Enterprise WordPress Solutions",
-        peragraph: "Pretium, a et sapien non nunc libero. Id dolor sed quis convallis turpis purus feugiat nibh eu. Auctor cras hac ac, sociis diam montes, turpis commodo elit.",
+        heading: "Small to Mid Size Business Development",
+        peragraph: "Et nunc fermentum id amet tempus, elementum. Eget ac ultrices tortor, mattis amet blandit aenean amet. Massa diam lacus, aliquam neque, cursus.",
         link: "Learn More",
     },
     {
         id: 3,
         img: headphone,
-        heading: "Enterprise WordPress Solutions",
-        peragraph: "Pretium, a et sapien non nunc libero. Id dolor sed quis convallis turpis purus feugiat nibh eu. Auctor cras hac ac, sociis diam montes, turpis commodo elit.",
+        heading: "Support & Maintenance",
+        peragraph: "Ac donec ipsum fringilla tortor, consectetur etiam sociis amet quis. Viverra volutpat pretium non in fusce tellus sed urna. Pharetra ullamcorper et donec aliquam.",
         link: "Learn More",
     },
 ]
